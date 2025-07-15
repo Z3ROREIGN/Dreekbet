80e2171f2824fb01b634f9d12752420b115d89f6
Z3ROREIGN <149897659+Z3ROREIGN@users.noreply.github.com>
2025-07-15 19:28:52 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   const code = req.query.code;
   if (!code) return res.status(400).json({ error: "Código não informado" });
